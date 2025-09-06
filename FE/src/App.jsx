a3dee058fe607ad911ba83b2bd30dc339404164a
aradhyacp <cparadhya@proton.me>
2025-09-07 00:34:38 +0530

    routing add

--- FE/src/App.jsx
+++ FE/src/App.jsx
@@ -1,21 +1,13 @@
 import { useState } from 'react'
 import './App.css'
 import { SignedIn, SignedOut, SignInButton, UserButton } from '@clerk/clerk-react';
+import AppRoutes from './routes/AppRoutes';
 
 
 function App() {
 
   return (
-    <div className="">
-      <header>
-      <SignedOut>
-        <SignInButton />
-      </SignedOut>
-      <SignedIn>
-        <UserButton />
-      </SignedIn>
-    </header>
-    </div>
+    <AppRoutes/>
   )
 }
 
