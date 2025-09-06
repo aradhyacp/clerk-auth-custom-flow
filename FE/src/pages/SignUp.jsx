dd5aac9d051b84b4392af327f4937e18a09a0a36
aradhyacp <cparadhya@proton.me>
2025-09-07 00:35:16 +0530

    created signup with clerk custom flow

---
+++ FE/src/pages/SignUp.jsx
@@ -0,0 +1,283 @@
+import React, { useState } from "react";
+import { useSignUp } from "@clerk/clerk-react";
+import { useNavigate, Link } from "react-router-dom";
+import { Input } from "@/components/ui/input";
+import { Button } from "@/components/ui/button";
+import {
+  Card,
+  CardHeader,
+  CardTitle,
+  CardContent,
+  CardFooter,
+} from "@/components/ui/card";
+import { Label } from "@/components/ui/label";
+import { Alert, AlertDescription } from "@/components/ui/alert";
+import {
+  InputOTP,
+  InputOTPGroup,
+  InputOTPSeparator,
+  InputOTPSlot,
+} from "@/components/ui/input-otp";
+
+const SignUp = () => {
+  const { isLoaded, signUp, setActive } = useSignUp();
+  const [emailAddress, setEmailAddress] = useState("");
+  const [password, setPassword] = useState("");
+  const [pendingVerification, setPendingVerification] = useState(false);
+  const [verificationCode, setVerificationCode] = useState("");
+  const [error, setError] = useState(null);
+  const navigate = useNavigate();
+  const [showPassword, setShowPassword] = useState(false);
+  const [isloading, setIsLoading] = useState(false);
+
+  if (!isLoaded) {
+    return null;
+  }
+
+  const handleSignUp = async (e) => {
+    e.preventDefault();
+    if (!isLoaded) {
+      return null;
+    }
+
+    if (!emailAddress || !password) {
+      setError("Email and password are required.");
+      return;
+    }
+
+    setIsLoading(true);
+
+    try {
+      await signUp.create({
+        emailAddress: emailAddress,
+        password: password,
+      });
+
+      await signUp.prepareEmailAddressVerification({
+        strategy: "email_code",
+      });
+      setPendingVerification(true);
+    } catch (err) {
+      console.log(JSON.stringify(err, null, 2));
+      setError(err.errors[0].message);
+    } finally {
+      setIsLoading(false);
+    }
+  };
+  const onPressVerify = async (e) => {
+    e.preventDefault();
+
+    if (!isLoaded) {
+      return null;
+    }
+
+    setIsLoading(true);
+
+    try {
+      const otpVerify = await signUp.attemptEmailAddressVerification({
+        code: verificationCode,
+      });
+
+      if (otpVerify.status !== "complete") {
+        console.log(JSON.stringify(otpVerify, null, 2));
+      }
+
+      if (otpVerify.status === "complete") {
+        await setActive({ session: otpVerify.createdSessionId });
+        navigate("/dashboard");
+      }
+    } catch (err) {
+      console.error(JSON.stringify(err, null, 2));
+      setError(err.errors[0].message);
+    } finally {
+      setIsLoading(false);
+    }
+  };
+  return (
+    <div className="flex min-h-screen bg-gray-300 justify-center items-center flex-row">
+      <Card className="w-full max-w-md">
+        <CardHeader>
+          <CardTitle className="text-2xl font-bold text-center">
+            Sign Up for ACP.IO
+          </CardTitle>
+        </CardHeader>
+        <CardContent>
+          {!pendingVerification ? (
+            <form onSubmit={handleSignUp} className="flex flex-col gap-4">
+              <div className="space-y-2">
+                <Label>Email</Label>
+                <Input
+                  type="email"
+                  value={emailAddress}
+                  onChange={(e) => setEmailAddress(e.target.value)}
+                  required
+                />
+              </div>
+              <div className="space-y-2">
+                <Label>Password</Label>
+                <div className="relative">
+                  <Input
+                    type={showPassword ? "text" : "password"}
+                    value={password}
+                    onChange={(e) => {
+                      setPassword(e.target.value);
+                      error && setError(false);
+                    }}
+                    required
+                  />
+                  <button
+                    type="button"
+                    onClick={() => setShowPassword((prev) => !prev)}
+                    className="absolute right-2 top-1/2 -translate-y-1/2"
+                  >
+                    {showPassword ? (
+                      <svg
+                        xmlns="http://www.w3.org/2000/svg"
+                        viewBox="0 0 24 24"
+                        fill="none"
+                        stroke="currentColor"
+                        stroke-width="2"
+                        stroke-linecap="round"
+                        stroke-linejoin="round"
+                        class="lucide lucide-eye-icon lucide-eye h-5 w-5"
+                      >
+                        <path d="M2.062 12.348a1 1 0 0 1 0-.696 10.75 10.75 0 0 1 19.876 0 1 1 0 0 1 0 .696 10.75 10.75 0 0 1-19.876 0" />
+                        <circle cx="12" cy="12" r="3" />
+                      </svg>
+                    ) : (
+                      <svg
+                        xmlns="http://www.w3.org/2000/svg"
+                        width="24"
+                        height="24"
+                        viewBox="0 0 24 24"
+                        fill="none"
+                        stroke="currentColor"
+                        stroke-width="2"
+                        stroke-linecap="round"
+                        stroke-linejoin="round"
+                        class="lucide lucide-eye-off-icon lucide-eye-off h-5 w-5"
+                      >
+                        <path d="M10.733 5.076a10.744 10.744 0 0 1 11.205 6.575 1 1 0 0 1 0 .696 10.747 10.747 0 0 1-1.444 2.49" />
+                        <path d="M14.084 14.158a3 3 0 0 1-4.242-4.242" />
+                        <path d="M17.479 17.499a10.75 10.75 0 0 1-15.417-5.151 1 1 0 0 1 0-.696 10.75 10.75 0 0 1 4.446-5.143" />
+                        <path d="m2 2 20 20" />
+                      </svg>
+                    )}
+                  </button>
+                </div>
+              </div>
+              {error && (
+                <Alert variant="destructive">
+                  <AlertDescription>{error}</AlertDescription>
+                </Alert>
+              )}
+              {isloading ? (
+                <Button>
+                  <div className="flex items-center gap-3">
+                    Please wait
+                    <svg
+                      xmlns="http://www.w3.org/2000/svg"
+                      width="24"
+                      height="24"
+                      viewBox="0 0 24 24"
+                      fill="none"
+                      stroke="currentColor"
+                      stroke-width="2"
+                      stroke-linecap="round"
+                      stroke-linejoin="round"
+                      class="lucide lucide-loader-icon lucide-loader spin"
+                    >
+                      <path d="M12 2v4" />
+                      <path d="m16.2 7.8 2.9-2.9" />
+                      <path d="M18 12h4" />
+                      <path d="m16.2 16.2 2.9 2.9" />
+                      <path d="M12 18v4" />
+                      <path d="m4.9 19.1 2.9-2.9" />
+                      <path d="M2 12h4" />
+                      <path d="m4.9 4.9 2.9 2.9" />
+                    </svg>
+                  </div>
+                </Button>
+              ) : (
+                <Button type="submit">Sign Up</Button>
+              )}
+            </form>
+          ) : (
+            <form onSubmit={onPressVerify} className="space-y-4">
+              <div className="flex flex-col items-center gap-3">
+                <Label>Verification Code</Label>
+                <InputOTP
+                  maxLength={6}
+                  onChange={(e) => setVerificationCode(e)}
+                  value={verificationCode}
+                >
+                  <InputOTPGroup>
+                    <InputOTPSlot index={0} />
+                    <InputOTPSlot index={1} />
+                    <InputOTPSlot index={2} />
+                  </InputOTPGroup>
+                  <InputOTPSeparator />
+                  <InputOTPGroup>
+                    <InputOTPSlot index={3} />
+                    <InputOTPSlot index={4} />
+                    <InputOTPSlot index={5} />
+                  </InputOTPGroup>
+                </InputOTP>
+              </div>
+              {error && (
+                <Alert variant="destructive">
+                  <AlertDescription>{error}</AlertDescription>
+                </Alert>
+              )}
+              {isloading ? (
+                <Button className="w-full">
+                  <div className="flex gap-3 items-center">
+                    Please wait
+                    <svg
+                      xmlns="http://www.w3.org/2000/svg"
+                      width="24"
+                      height="24"
+                      viewBox="0 0 24 24"
+                      fill="none"
+                      stroke="currentColor"
+                      stroke-width="2"
+                      stroke-linecap="round"
+                      stroke-linejoin="round"
+                      class="lucide lucide-loader-icon lucide-loader spin"
+                    >
+                      <path d="M12 2v4" />
+                      <path d="m16.2 7.8 2.9-2.9" />
+                      <path d="M18 12h4" />
+                      <path d="m16.2 16.2 2.9 2.9" />
+                      <path d="M12 18v4" />
+                      <path d="m4.9 19.1 2.9-2.9" />
+                      <path d="M2 12h4" />
+                      <path d="m4.9 4.9 2.9 2.9" />
+                    </svg>
+                  </div>
+                </Button>
+              ) : (
+                <Button type="submit" className="w-full">Verify Email</Button>
+              )}
+            </form>
+          )}
+        </CardContent>
+        <CardFooter className="justify-center">
+          <div className="flex gap-3 flex-row items-center">
+            <div className="text-sm text-[hsl(0,0%,9%)]">
+              Already have an account?
+            </div>
+            <Link
+              to="/login"
+              className="font-medium text-primary hover:underline"
+            >
+              Login
+            </Link>
+          </div>
+        </CardFooter>
+      </Card>
+    </div>
+  );
+};
+
+export default SignUp;
