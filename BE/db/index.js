95edeecdeb3e4b5fe60e11cfd7c823741c91d331
aradhyacp <cparadhya@proton.me>
2025-09-09 19:43:24 +0530

    root changes

---
+++ BE/db/index.js
@@ -0,0 +1,9 @@
+import { createClient } from "@supabase/supabase-js";
+import config from "../config.js";
+
+const SUPABASE_URL = config.SUPABASE_URL;
+const SUPABASE_KEY = config.SUPABASE_KEY;
+
+const supabase = createClient(SUPABASE_URL, SUPABASE_KEY);
+
+export default supabase
